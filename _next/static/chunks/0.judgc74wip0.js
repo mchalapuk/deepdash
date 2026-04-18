021cc053ed72f0cf371313fddd8669c1b0f02b39
Maciej Chałapuk <maciej@chalapuk.pl>
2026-04-18 11:27:13 +0200

    Deploy static build (2026-04-18T09:27:13Z)

---
+++ _next/static/chunks/0.judgc74wip0.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,15331,t=>{t.v(s=>Promise.all(["static/chunks/0tjw53xvazug1.js"].map(s=>t.l(s))).then(()=>s(48661)))},36405,t=>{t.v(s=>Promise.all(["static/chunks/06jfrknt3ahre.js"].map(s=>t.l(s))).then(()=>s(60160)))}]);
